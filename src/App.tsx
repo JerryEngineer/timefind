c57b3ba05041740729d167e5ed50da272c6a8d21
Jerry Zhang <7928617+JerryBuilds@users.noreply.github.com>
2026-08-08 16:51:23 -0700

    added a feedback submission button that links to a github issue

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,22 @@ import { EventPage } from "./pages/EventPage";
 export default function App() {
   return (
     <div className="app">
-      <a
-        href="https://github.com/JerryEngineer/timefind#how-it-works"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="help-link"
-      >
-        Help
-      </a>
+      <div className="corner-links">
+        <a
+          href="https://github.com/JerryEngineer/timefind#how-it-works"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Help
+        </a>
+        <a
+          href="https://github.com/JerryEngineer/timefind/issues/new?template=feedback.yml"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Feedback
+        </a>
+      </div>
       <Routes>
         <Route path="/" element={<CreateEventPage />} />
         <Route path="/:id" element={<EventPage />} />
